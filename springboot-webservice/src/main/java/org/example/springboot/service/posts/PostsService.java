78e630b4214175fb1c112d14fa35aab632881110
zz132456zz <zz132456zz@kakao.com>
2021-10-25 21:23:23 +0900

    등록 API

---
+++ springboot-webservice/src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package org.example.springboot.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ springboot-webservice/src/main/java/org/example/springboot/web/PostsApiController.java
@@ -0,0 +1,18 @@
+package org.example.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.service.posts.PostsService;
+import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
