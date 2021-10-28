d7257022254d05bf02eacae31f1090a4ece3a849
zz132456zz <zz132456zz@kakao.com>
2021-10-29 00:15:15 +0900

    게시글 수정

--- springboot-webservice/src/main/java/org/example/springboot/web/IndexController.java
+++ springboot-webservice/src/main/java/org/example/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package org.example.springboot.web;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.service.posts.PostsService;
+import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,6 +24,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
--- springboot-webservice/src/main/resources/static/js/app/index.js
+++ springboot-webservice/src/main/resources/static/js/app/index.js
@@ -4,6 +4,9 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () {  // btn-update란 id를 가진 HTML 엘리면트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트를 등록한다.
+            _this.update();
+        });
     },
     save : function () {
         var data = {
@@ -21,6 +24,27 @@ var main = {
         }).done(function () {
             alert('글이 등록되었습니다.');
             window.location.href = '/';  // 글 등록이 성공하면 메인페이지(/)로 이동한다.
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {  // update function
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',  // 여러 HTTP Method 중 PUT 메소드를 선택한다. PostsApiController에 있는 API에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다. 참고로 이는 REST 규약에 맞게 설정된 것이다.
+            url: '/api/v1/posts/'+id,  // 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가한다.
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
