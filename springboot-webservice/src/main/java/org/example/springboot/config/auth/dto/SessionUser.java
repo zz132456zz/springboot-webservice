e797edfe497d9201ec643213e8ad10c756c5d8a3
zz132456zz <zz132456zz@kakao.com>
2021-11-03 01:10:36 +0900

    네이버 로그인

--- springboot-webservice/src/main/java/org/example/springboot/config/auth/dto/SessionUser.java
+++ springboot-webservice/src/main/java/org/example/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package org.example.springboot.config.auth.dto;
 import lombok.Getter;
 import org.example.springboot.domain.user.User;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
